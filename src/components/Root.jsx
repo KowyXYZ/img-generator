54900dca8ad7395f9426491a91e545b9e8434ffc
KowyXYZ <kowybusiness@gmail.com>
2023-10-19 12:34:40 +0200

    removed api key beacuse openai is trash

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,7 +2,9 @@ import React, { useRef, useState } from 'react'
 import defImg from '../assets/ai.jpeg'
 import zsearch from '../assets/search.png'
 
+
 function Root() {
+
 
 
     const [imageUrl, setImageUrl] = useState('/')
@@ -18,7 +20,7 @@ function Root() {
                 method:'POST',
                 headers: {
                     "Content-Type": "application/json",
-                    Authorization: "Bearer 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",
+                    Authorization: "Bearer ",
 
                 },
                 body: JSON.stringify({
@@ -38,12 +40,12 @@ function Root() {
   return (
     <div className='container mx-auto flex items-center justify-center flex-col py-12 gap-8'>
             <p className='text-[34px] font-black'>AI IMAGE <span className='text-[purple]'>GENERATOR</span></p>
-            <img src={imageUrl==='/' ? defImg : imageUrl} alt="defimg" className='w-1/2 rounded-3xl' />
+            <img src={imageUrl==='/' ? defImg : imageUrl} alt="defimg" className='w-full sm:w-1/2 rounded-3xl' />
 
             <div className='flex justify-center items-center gap-2 mt-2 '>
-                <input ref={inputRef} type="text" className='outline-none rounded-full p-3 w-[500px] h-[60px] text-[18px]' placeholder='What do you want to see...'/>
+                <input ref={inputRef} type="text" className='outline-none rounded-full p-3 w-[300px] sm:w-[500px] h-[40px] sm:h-[60px] text-[18px]' placeholder='What do you want to see...'/>
                 <button onClick={() => handleSubmit()} className='border-2 rounded-3xl p-2 border-[purple] bg-[purple]'>
-                    <img className='w-8' src={zsearch} alt="search" />
+                    <img className='w-5 sm:w-8' src={zsearch} alt="search" />
                 </button>
             </div>
          
